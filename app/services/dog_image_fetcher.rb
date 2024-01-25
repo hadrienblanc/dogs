9616e6d12517899aa37db620f582adcff80c3bf9
Hadrien Blanc <hadrien.blanc.ext@melting-point.fr>
2024-01-25 17:57:46 +0100

    rewrite dog_image_fetcher.rb

--- app/services/dog_image_fetcher.rb
+++ app/services/dog_image_fetcher.rb
@@ -1,8 +1,7 @@
 require 'net/http'
-
-require 'net/http'
 require 'uri'
 require 'json'
+require 'cgi'
 
 class DogImageFetcher
   def initialize(dog_request)
@@ -10,28 +9,31 @@ class DogImageFetcher
   end
 
   def run
-    fetch_random_image
+    response = fetch_random_image
+    update_dog_request(response)
   end
 
   def fetch_random_image
-    parsed_response = get
-
-    if valid_url?(parsed_response['message']) 
-      @dog_request.update!(url: parsed_response['message'], responded_at: DateTime.now)
-    else
-      @dog_request.update!(error_message: parsed_response['message'], responded_at: DateTime.now)
-    end
+    response = Net::HTTP.get_response(uri)
+    parse_response(response)
+  rescue StandardError => e
+    { 'message' => e.message }
   end
 
-  def get
-    response = Net::HTTP.get_response(uri)
+  def parse_response(response)
     if response.is_a?(Net::HTTPSuccess)
       JSON.parse(response.body)
     else
-      { 'message' => "Unable to fetch image", status: response.code }
+      { 'message' => "Unable to fetch image", 'status' => response.code }
     end
-  rescue StandardError => e
-    { 'message' => e.message }
+  end
+
+  def update_dog_request(response)
+    if valid_url?(response['message'])
+      @dog_request.update!(url: response['message'], responded_at: Time.current)
+    else
+      @dog_request.update!(error_message: response['message'], responded_at: Time.current)
+    end
   end
 
   def valid_url?(url)
